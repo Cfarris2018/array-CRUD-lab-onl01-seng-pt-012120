64184e5eedadefe5c6bfdd6c951de6ea42332e0f
Camisha  Farris <cfarris@anointedflooring.com>
2020-02-03 16:10:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
-  
+  emotions = []
 end
 
 def create_an_array
-  
+  emotions = ["new", "exciting", "challenging", "rewarding"]
 end
 
 def add_element_to_end_of_array(array, element)
   
+   array << "annoying"
+   
 end
 
 def add_element_to_start_of_array(array, element)
   
-end
-
-def remove_element_from_end_of_array(array)
+  wisdom = array.unshift 
   
 end
 
+def remove_element_from_end_of_array(array)
+   
+   annoying = array.pop 
+   
+end
+
 def remove_element_from_start_of_array(array)
+
+
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+ [1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  [5]
 end
